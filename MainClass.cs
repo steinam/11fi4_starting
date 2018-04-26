acae99e716abb758f0b2d6540cb9d2bb50eac6d0
Karl Steinam <webmaster@steinam.de>
2018-04-26 10:50:23 +0200

    First Update condsole.writeLine

--- MainClass.cs
+++ MainClass.cs
@@ -13,6 +13,10 @@ namespace StartingPoint
 		[STAThread]
 		static void Main(string[] args)
 		{
+
+
+            Console.WriteLine("Hello Wordl");
+
 			// Create movies
 			Movie movCinderella = new Movie("Cinderella", PriceCodes.Childrens);
 			Movie movStarWars = new Movie("Star Wars", PriceCodes.Regular);
